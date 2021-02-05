a84f575e973fe6e3c83c61eab5c95e40f41bff53
Stanislav Timchenko <st.timchenko@gmail.com>
2021-02-05 15:20:04 +0800

    Create ability to enable and disable widgets creation.

--- app/src/main/kotlin/dev/sttimchenko/modernwidgetplayground/MainActivity.kt
+++ app/src/main/kotlin/dev/sttimchenko/modernwidgetplayground/MainActivity.kt
@@ -2,8 +2,10 @@ package dev.sttimchenko.modernwidgetplayground
 
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dev.sttimchenko.widgetworkmanagerretrofit.BitcoinWidgetProvider
 
@@ -17,9 +19,15 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.button_update_widgets)
     }
 
+    private val widgetStatusButton: Button by lazy(LazyThreadSafetyMode.NONE) {
+        findViewById(R.id.button_widget_status)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        updateWidgetsStatusButtonText()
     }
 
     override fun onResume() {
@@ -32,6 +40,11 @@ class MainActivity : AppCompatActivity() {
         updateWidgetsButton.setOnClickListener {
             triggerWidgetsUpdate()
         }
+
+        widgetStatusButton.setOnClickListener {
+            setWidgetsEnabled(areWidgetsEnabled().not())
+            updateWidgetsStatusButtonText()
+        }
     }
 
     override fun onPause() {
@@ -39,18 +52,63 @@ class MainActivity : AppCompatActivity() {
 
         pinWidgetButton.setOnClickListener(null)
         updateWidgetsButton.setOnClickListener(null)
+        widgetStatusButton.setOnClickListener(null)
     }
 
     private fun requestToPinWidget() {
         val appWidgetManager = getSystemService(AppWidgetManager::class.java)
-        val myProvider = ComponentName(this, BitcoinWidgetProvider::class.java)
+        val providerComponent = ComponentName(this, BitcoinWidgetProvider::class.java)
 
         if (appWidgetManager.isRequestPinAppWidgetSupported) {
-            appWidgetManager.requestPinAppWidget(myProvider, null, null)
+            appWidgetManager.requestPinAppWidget(providerComponent, null, null)
         }
     }
 
     private fun triggerWidgetsUpdate() {
         sendBroadcast(BitcoinWidgetProvider.refreshIntent(this))
+    }
+
+    private fun areWidgetsEnabled(): Boolean {
+        val providerComponent = ComponentName(this, BitcoinWidgetProvider::class.java)
+        val enabledSetting = packageManager.getComponentEnabledSetting(providerComponent)
+
+        return enabledSetting == PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                || enabledSetting == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT
+    }
+
+    private fun setWidgetsEnabled(enabled: Boolean) {
+        val appWidgetManager = getSystemService(AppWidgetManager::class.java)
+        val providerComponent = ComponentName(this, BitcoinWidgetProvider::class.java)
+        val appWidgetIds = appWidgetManager.getAppWidgetIds(providerComponent)
+
+        when {
+            enabled -> packageManager.setComponentEnabledSetting(
+                providerComponent,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP
+            )
+
+            enabled.not() && appWidgetIds.isNotEmpty() -> Toast.makeText(
+                this,
+                "Cannot disable widgets provider as it will compromise added widgets!",
+                Toast.LENGTH_LONG
+            ).show()
+
+            enabled.not() -> packageManager.setComponentEnabledSetting(
+                providerComponent,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP
+            )
+        }
+    }
+
+    private fun updateWidgetsStatusButtonText() {
+        val widgetStatus = if (areWidgetsEnabled()) {
+            "Disable widgets"
+        } else {
+            "Enable widgets"
+        }
+
+        widgetStatusButton.text = widgetStatus
     }
 }
